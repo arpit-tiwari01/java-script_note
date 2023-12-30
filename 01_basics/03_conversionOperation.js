010754f7200b5a6884d7cd1929b1759788ec865d
Arpit Tiwari <123574152+arpit-tiwari01@users.noreply.github.com>
2023-12-30 12:04:16 +0000

    conversuin operatios

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+let score = "33abc" //try--> 33,string,boolean,undefinde
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(valueInNumber);
+
+// "33"=>33 string
+// "33abc"=>NaN (not a number) -number
+// true=>1,false=>0
+
+let isLoggedIn=true
+let booleanIsloggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsloggedIn);
+
+let somename=33
+let stringNumber=String(somename)
+console.log(typeof stringNumber);

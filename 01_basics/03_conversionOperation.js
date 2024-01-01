b830725d29cdf2432d08d2d10ed546d6f4a8b7e2
Arpit Tiwari <123574152+arpit-tiwari01@users.noreply.github.com>
2024-01-01 15:30:20 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -16,4 +16,42 @@ let booleanIsloggedIn=Boolean(isLoggedIn)
 
 let somename=33
 let stringNumber=String(somename)
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ***************** Operations *********************
+
+let value = 3
+let negvalue = -3
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " Coder's"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+"2");
+//not good prectice
+console.log("1"+2+2);   //122
+console.log(1+2+"2");   //32
+// code readability is important
+console.log("1"+(2+2));
+
+console.log(+true);   //->this is confusing code
+
+let gameCounter = 100
+++gameCounter
+//gameCounter++  ->try this also 
+console.log(gameCounter);
+
+// note: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
